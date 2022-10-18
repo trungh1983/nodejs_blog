ccea5758ff655c3a323c033fccee47808105e663
thai trung <trungh1983>
2022-10-18 10:44:09 +0700

    commit change

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) =>{
-    var a = 1;
-    var b = 5;
-
-    var c= a+b;
-    return res.send('Hello World');
+app.use(morgan('combined'))
+app.get('/', (req, res) =>{
+   return res.send('Hello World');
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
